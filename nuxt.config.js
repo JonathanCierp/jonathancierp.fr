d73b24b9bd7995487b383b29e9c4c14c4b8ac105
Jonathan CIERP <cierp.jonathan@gmail.com>
2020-07-17 19:34:31 +0200

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,86 @@
+//npm i && npx nuxt build && npx nuxt export && npx nuxt serve
+//nuxt build && nuxt export && nuxt serve
+export default {
+	server: {
+		port: 3000, // par défaut: 3000
+		host: '0.0.0.0' // par défaut: localhost
+	},
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'universal',
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
+  plugins: [
+  ],
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {},
+	pwa: {
+		meta: {
+			title: 'My PWA',
+			author: 'Me',
+		},
+		manifest: {
+			name: 'Nuxt.js PWAs are so easy',
+			short_name: 'Nuxt.js PWA',
+			lang: 'fr',
+		},
+	},
+	/*
+	** Build configuration
+	** See https://nuxtjs.org/api/configuration-build/
+	*/
+  build: {
+  }
+}
