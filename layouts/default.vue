f1a7b831a948fee6c4966aeb5d7fdbdc3f832a2f
Jonathan CIERP <cierp.jonathan@gmail.com>
2020-07-17 20:14:47 +0200

    feat(header): background image, brand image and text

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,38 @@
 <template>
   <div>
+		<div class="header-background-color">
+			<div class="header-background-image absolute w-full"></div>
+			<header class="nav container mx-auto flex justify-between text-white py-4">
+				<div class="nav__brand flex items-center">
+					<div class="nav__brand_image w-16 h-16 rounded-full"></div>
+					<div class="nav__brand__text">
+						<span class="font-semibold">Jonathan CIERP</span>
+					</div>
+				</div>
+				<div class="nav__menu">
+					b
+				</div>
+			</header>
+		</div>
     <Nuxt />
   </div>
 </template>
 
 <style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+	.header-background-color, .header-background-image {
+		height: 70vh;
+	}
+	.header-background-color {
+		background-color: #575FCF;
+	}
+	.header-background-image {
+		background-position: 60% 60%;
+		background-image: url("~@/assets/img/background.jpg");
+		opacity: .2;
+	}
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+	.nav .nav__brand .nav__brand_image {
+		background-position: 60% 60%;
+		background-image: url("~@/assets/img/background.jpg");
+	}
 </style>
