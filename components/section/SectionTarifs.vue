c8574a7b97443b8cbc9bf3fd0b455c5f21d859a0
Jonathan CIERP <cierp.jonathan@gmail.com>
2020-07-21 00:07:56 +0200

    feat : section tarifs, card, content

--- components/section/SectionTarifs.vue
+++ components/section/SectionTarifs.vue
@@ -1,12 +1,230 @@
 <template>
-	<div>
-		c
+	<div class="tarifs flex mt-16">
+		<section-tarifs-item v-for="(tarif, i) in tarifs" :key="i" :tarif="tarif" />
+		<div class="tarifs__row w-1/4 flex flex-col">
+			<section-tarifs-item class="w-full mb-4" :tarif="tarifMaintenance" :sur-mesure="false" />
+			<section-tarifs-item class="w-full" :tarif="tarifSpecifique" :sur-mesure="false" />
+		</div>
 	</div>
 </template>
 
 <script>
+	import SectionTarifsItem from "@/components/section/SectionTarifsItem"
+	import IconPlus from "@/components/icon/IconPlus"
+
 	export default {
-		name: "section-tarifs"
+		name: "section-tarifs",
+		components: {
+			SectionTarifsItem
+		},
+		data() {
+			return {
+				tarifs: [
+					{
+						title: "Intégration web",
+						subtitle: "Site OnePage",
+						priceText: "À partir de",
+						price: "899",
+						priceSup: "*",
+						priceSupDevise: "€",
+						buttonLabel: "Demander un devis",
+						details: [
+							{
+								icon: IconPlus,
+								title: "Charte graphique à partir d'une maquette",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "1 page",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Interface administrateur STANDARD",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Mise en place du contenu (textes & images)",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Formulaire de contact",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Site responsive (adapté tous supports)",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Optimisation du référencement",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: " Statistiques de visite",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Formation utilisation du site",
+								class: "font-bold"
+							}
+						]
+					},
+					{
+						title: "Intégration web",
+						subtitle: "Site vitrine",
+						priceText: "À partir de",
+						price: "1200",
+						priceSup: "**",
+						priceSupDevise: "€",
+						buttonLabel: "Demander un devis",
+						details: [
+							{
+								icon: IconPlus,
+								title: "Charte graphique à partir d'une maquette",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "5 pages",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Interface administrateur SUR-MESURE",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Mise en place du contenu (textes & images)",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Formulaire de contact & map",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Site responsive (adapté tous supports)",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Optimisation du référencement et des performances ",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: " Statistiques de visite",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Formation utilisation du site",
+								class: "font-bold"
+							}
+						]
+					},
+					{
+						title: "Plateforme",
+						subtitle: "E-commerce",
+						priceText: "À partir de",
+						price: "2500",
+						priceSup: "***",
+						priceSupDevise: "€",
+						buttonLabel: "Demander un devis",
+						details: [
+							{
+								icon: IconPlus,
+								title: "Charte graphique à partir d'une maquette",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Paiement sécurisé",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Interface administrateur SUR-MESURE",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Mise en place du contenu (textes & images)",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Formulaire de contact & map",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Site responsive (adapté tous supports)",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Optimisation du référencement et des performances ",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: " Statistiques de visite",
+								class: ""
+							},
+							{
+								icon: IconPlus,
+								title: "Formation utilisation du site",
+								class: "font-bold"
+							}
+						]
+					},
+				],
+				tarifMaintenance: {
+					title: "Forfait",
+					subtitle: "maintenance",
+					price: "300",
+					priceSup: "****",
+					priceSupDevise: "€ / an",
+					buttonLabel: "Me contacter",
+					details: [
+						{
+							icon: IconPlus,
+							title: "Mises à jour site internet",
+							class: ""
+						},
+						{
+							icon: IconPlus,
+							title: "Petites modifications graphiques ou de contenu",
+							class: ""
+						},
+						{
+							icon: IconPlus,
+							title: "Optimisation performances",
+							class: ""
+						}
+					]
+				},
+				tarifSpecifique: {
+					title: "Développements",
+					subtitle: "spécifiques",
+					priceText: "Taux journalier moyen",
+					price: "300",
+					priceSup: "******",
+					priceSupDevise: "€ / Jour",
+					buttonLabel: "Me contacter"
+				}
+			}
+		}
 	}
 </script>
 
---
+++ components/section/SectionTarifsItem.vue
@@ -0,0 +1,94 @@
+<template>
+	<div class="tarifs__item bg-white w-1/4 mr-6 flex flex-col items-center">
+		<div class="tarifs__item__title font-bold text-3xl">
+			<h2>{{ tarif.title }}</h2>
+		</div>
+		<div class="tarifs__item__subtitle font-bold text-2xl">
+			<h3>{{ tarif.subtitle }}</h3>
+		</div>
+		<div class="tarifs__item__separator my-4"></div>
+		<div class="tarifs__item__price-text">
+			<div v-if="tarif.title === 'Forfait'" class="flex">
+				<span class="font-bold text-xl">35</span>
+				<span class="tarifs__item__price__devise font-bold ml-1">€ / heure</span>
+				<span class="tarifs__item__price__sup ml-2">{{ tarif.priceSup }}*</span>
+				<span class="font-bold text-xl ml-2">Ou</span>
+			</div>
+			<span v-else>{{ tarif.priceText }}</span>
+		</div>
+		<div class="tarifs__item__price flex">
+			<div class="tarifs__item__price__text font-bold text-5xl">
+				<h3>{{ tarif.price }}</h3>
+			</div>
+			<div class="tarifs__item__price__devise font-bold text-xl mt-2">
+				<span>{{ tarif.priceSupDevise }}</span>
+			</div>
+			<div class="tarifs__item__price__sup ml-2 mt-2">
+				<span>{{ tarif.priceSup }}</span>
+			</div>
+		</div>
+		<div class="tarifs__item__separator" ></div>
+		<div v-if="surMesure" class="tarifs__item__detail text-center text-sm font-semibold">
+			<span>Développé SUR-MESURE</span> <br>
+			<span>(Codage à la main)</span>
+		</div>
+		<div class="tarifs__item__details w-full mt-4">
+			<div class="tarifs__item__details__item flex items-center h-12" v-for="detail in tarif.details" :key="detail.title">
+				<div class="tarifs__item__details__item__icon w-1/5 text-center">
+					<component class="inline" :is="detail.icon" />
+				</div>
+				<div class="tarifs__item__details__item__text w-4/5" :class="detail.class">
+					{{ detail.title }}
+				</div>
+			</div>
+		</div>
+		<div class="tarifs__item__button" :class="`${surMesure ? 'my-10' : 'my-5'}`">
+			<button class="text-white font-semibold py-3 px-8">{{ tarif.buttonLabel }}</button>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "section-tarifs-item",
+		props: {
+			tarif: {
+				type: Object,
+				required: true
+			},
+			surMesure: {
+				type: Boolean,
+				default: false
+			}
+		}
+	}
+</script>
+
+<style>
+	.tarifs__item .tarifs__item__separator {
+		height: 2px;
+		background-color: #1ABC9C;
+		width: 50%;
+	}
+
+	.tarifs__item .tarifs__item__price-text {
+		color: #818181;
+	}
+
+	.tarifs__item .tarifs__item__details .tarifs__item__details__item__text {
+		font-size: .85rem;
+	}
+
+	.tarifs__item__details__item:nth-child(odd){
+		background-color: #F1F1F1;
+	}
+
+	.tarifs .tarifs__item .tarifs__item__button button {
+		background-color: #1ABC9C;
+		border: 4px solid #1ED0AD;
+	}
+
+	.tarifs .tarifs__item .tarifs__item__button button:hover {
+		background-color: #1ED0AD;
+	}
+</style>
